9f81f2506b6c67ec359aba7930000216ab09b96a
Jonas Sävås <jonas.savas123@gmail.com>
2025-11-07 15:13:48 +0100

    Adds a create method for authors in the database

--- src/main/java/com/jonassavas/spring_library_database/dao/AuthorDao.java
+++ src/main/java/com/jonassavas/spring_library_database/dao/AuthorDao.java
@@ -1,5 +1,7 @@
 package com.jonassavas.spring_library_database.dao;
 
+import com.jonassavas.spring_library_database.domain.Author;
+
 public interface AuthorDao {
-    
+    void create(Author author);
 }
--- src/main/java/com/jonassavas/spring_library_database/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/jonassavas/spring_library_database/dao/impl/AuthorDaoImpl.java
@@ -3,6 +3,7 @@ package com.jonassavas.spring_library_database.dao.impl;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.jonassavas.spring_library_database.dao.AuthorDao;
+import com.jonassavas.spring_library_database.domain.Author;
 
 public class AuthorDaoImpl implements AuthorDao {
 
@@ -12,4 +13,10 @@ public class AuthorDaoImpl implements AuthorDao {
         this.jdbcTemplate = jdbcTemplate;
     }
     
+    @Override
+    public void create(Author author){
+        jdbcTemplate.update("INSERT INTO authors (id, name, age) VALUES (?, ?, ?)",
+        author.getId(), author.getName(), author.getAge());
+    }
+
 }
