8a41b1c1a9b0f09695503a5329b92e535ae87df9
Jonas Sävås <jonas.savas123@gmail.com>
2025-11-11 21:04:31 +0100

    Adds a find method for the books

--- src/main/java/com/jonassavas/spring_library_database/dao/BookDao.java
+++ src/main/java/com/jonassavas/spring_library_database/dao/BookDao.java
@@ -1,10 +1,12 @@
 package com.jonassavas.spring_library_database.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.jonassavas.spring_library_database.domain.Book;
 
 public interface BookDao {
     void create(Book book);
-    Optional<Book> find(String isbn);
+    Optional<Book> findOne(String isbn);
+    List<Book> find();
 }
--- src/test/java/com/jonassavas/spring_library_database/dao/impl/BookDaoImplIntegrationTests.java
+++ src/test/java/com/jonassavas/spring_library_database/dao/impl/BookDaoImplIntegrationTests.java
@@ -1,11 +1,13 @@
 package com.jonassavas.spring_library_database.dao.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.jonassavas.spring_library_database.TestDataUtil;
@@ -17,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class BookDaoImplIntegrationTests {
     
     private AuthorDao authorDao;
@@ -32,11 +35,34 @@ public class BookDaoImplIntegrationTests {
     public void testThatBookCanBeCreatedAndRecalled(){
         Author author = TestDataUtil.createTestAuthorA();
         authorDao.create(author);
-        Book book = TestDataUtil.createTestBook();
+        Book book = TestDataUtil.createTestBookA();
         book.setAuthorId(author.getId());
         underTest.create(book);
-        Optional<Book> result = underTest.find(book.getIsbn());
+        Optional<Book> result = underTest.findOne(book.getIsbn());
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(book);
+    }
+
+    @Test
+    public void testThatMultipleBooksCanBeCreatedAndRecalled(){
+        Author author = TestDataUtil.createTestAuthorA();
+        authorDao.create(author);
+
+        Book bookA = TestDataUtil.createTestBookA();
+        bookA.setAuthorId(author.getId());
+        underTest.create(bookA);
+
+        Book bookB = TestDataUtil.createTestBookB();
+        bookB.setAuthorId(author.getId());
+        underTest.create(bookB);
+
+        Book bookC = TestDataUtil.createTestBookC();
+        bookC.setAuthorId(author.getId());
+        underTest.create(bookC);
+
+        List<Book> result = underTest.find();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(bookA, bookB, bookC);
     }
 }
